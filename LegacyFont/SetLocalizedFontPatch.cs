3745bc20d6c65506e1e4b758324612ad821d583e
tjwogud <rlatjsal78@gmail.com>
2022-05-13 17:05:11 +0900

    add function

---
+++ LegacyFont/SetLocalizedFontPatch.cs
@@ -0,0 +1,24 @@
+﻿using BackToThePast.Utils;
+using HarmonyLib;
+using UnityEngine.UI;
+
+namespace BackToThePast.LegacyFont
+{
+    [HarmonyPatch(typeof(RDString), "SetLocalizedFont", typeof(Text))]
+    public static class SetLocalizedFontPatch
+    {
+        private static FontData prev;
+
+        public static void Prefix(Text text)
+        {
+            prev = RDString.fontData;
+            if (Main.Settings.legacyFont && ((Main.Settings.butNotTitle && text.name == "txtLevelName") || (Main.Settings.butNotSetting && text.transform.root.name == "PauseMenu(Clone)")))
+                typeof(RDString).Set("fontData", Main.font);
+        }
+
+        public static void Postfix()
+        {
+            typeof(RDString).Set("fontData", prev);
+        }
+    }
+}
