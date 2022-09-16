cd41e5df78ac543d77e928fe2b5ea9b9fee55a47
tjwogud <rlatjsal78@gmail.com>
2022-09-16 21:47:49 +0900

    bugfix

--- OldXO/JumpToWorldPortalPatch.cs
+++ OldXO/JumpToWorldPortalPatch.cs
@@ -7,19 +7,24 @@ namespace BackToThePast.OldXO
     [BTTPPatch(typeof(scnLevelSelect), "JumpToWorldPortal")]
     public static class JumpToWorldPortalPatch
     {
-        public static bool Prefix(string world, bool instant, bool wipeFirst, scnLevelSelect __instance)
+        public static bool Prefix(string world, ref bool wipeFirst, scnLevelSelect __instance)
         {
             scrPlanet chosenplanet = scrController.instance.chosenplanet;
             float planetX = chosenplanet.transform.position.x;
             float planetY = chosenplanet.transform.position.y;
             Transform transform = chosenplanet.transform;
+            if (world == null && planetX == 15 && planetY == 23)
+            {
+                transform.LocalMoveXY(0, 23);
+                return true;
+            }
             if ((PlayerPrefs.GetInt("BackToThePast.FoundSecretIsland", 0) == 1 && !wipeFirst && world == null && planetX == 0 && planetY == 23) || world == "BackToThePast.SecretIsland")
-			{
-				if (wipeFirst)
-					scrUIController.instance.WipeToBlack(WipeDirection.StartsFromRight, delegate
-					{
-						__instance.Set("responsive", true);
-						__instance.JumpToWorldPortal(world, true, false);
+            {
+                if (wipeFirst)
+                    scrUIController.instance.WipeToBlack(WipeDirection.StartsFromRight, delegate
+                    {
+                        __instance.Set("responsive", true);
+                        __instance.JumpToWorldPortal(world, true, false);
                         scrUIController.instance.WipeFromBlack();
                         foreach (Collider2D collider in Physics2D.OverlapPointAll(new Vector2(planetX, planetY), 1 << LayerMask.NameToLayer("Floor")))
                         {
@@ -32,19 +37,15 @@ namespace BackToThePast.OldXO
                 {
                     scrCamera.instance.SetXOffset(0);
                     scrCamera.instance.SetYOffset(0);
-					transform.LocalMoveXY(15, 23);
-					__instance.Set("menuPhase", 1);
-					if (instant)
-						scrCamera.instance.ViewObjectInstant(transform, true);
-					else
-						scrCamera.instance.Refocus(transform);
-                    scrCamera.instance.isMoveTweening = true;
+                    transform.LocalMoveXY(15, 23);
+                    __instance.Set("menuPhase", 1);
                     scrCamera.instance.positionState = (PositionState)1001;
-                    scrCamera.instance.ViewVectorInstant(new Vector2(15, 24), true);
+                    scrCamera.instance.isMoveTweening = true;
+                    scrCamera.instance.Refocus(transform);
                 }
-				return false;
+                return false;
             }
-			return true;
+            return true;
         }
     }
 }
